b08b0e8c81303027b3709d1618e3f72bd9c2ec27
Increasingly unclear <kevinwalker1@mac.com>
2023-06-06 14:30:08 +0100

    Create pose.js

---
+++ pose/pose.js
@@ -0,0 +1,96 @@
+// ML POSE DETECTION IN IMAGES
+// BY KEVIN WALKER JUNE 2023
+// ADAPTED FROM https://learn.ml5js.org/#/reference/posenet
+// MY IMPLEMENTATION AT https://ai.postdigitalcultures.org/pose.html
+
+let img; // the image to detect
+let poseNet; // holds the model
+let poses = [];
+
+function setup() {
+    // I kept same size canvas and images to keep everything registered
+    let myCanvas = createCanvas(640, 480);
+    myCanvas.parent('myContainer'); // div specified in the HTML
+    // Select a random numbered image from folder
+    imgsrc = "images/fdp63/fdp00" + Math.floor(Math.random() * 130 + 1) + ".png";
+    img = createImg(imgsrc, imageReady); // wait til image is loaded before detecting
+    img.size(width, height); // scale to canvas size
+    img.hide(); // hide the image in the browser
+    frameRate(1); // set the frameRate to 1 since we don't need it to be running quickly in this case
+}
+
+// when the image is ready, then load up poseNet
+function imageReady(){
+    // set some options
+    let options = {
+        imageScaleFactor: 1,
+        minConfidence: 0.1
+    }
+    
+    // assign poseNet
+    poseNet = ml5.poseNet(modelReady, options);
+    // This sets up an event that listens to 'pose' events
+    poseNet.on('pose', function (results) {
+        poses = results;
+    });
+}
+
+// when poseNet is ready, do the detection
+function modelReady() {
+    select('#status').html('Model Loaded');
+     
+    // When the model is ready, run the singlePose() function...
+    // If/When a pose is detected, poseNet.on('pose', ...) will be listening for the detection results 
+    // in the draw() loop, if there are any poses, then carry out the draw commands
+    poseNet.singlePose(img)
+}
+
+// draw() will not show anything until poses are found
+function draw() {
+    if (poses.length > 0) {
+        image(img, 0, 0, width, height);
+        drawSkeleton(poses);
+        drawKeypoints(poses);
+        noLoop(); // stop looping when the poses are estimated
+    }
+
+}
+
+// The following comes from https://ml5js.org/docs/posenet-webcam
+// A function to draw ellipses over the detected keypoints
+
+function drawKeypoints() {
+    // Loop through all the poses detected
+    for (let i = 0; i < poses.length; i++) {
+        // For each pose detected, loop through all the keypoints
+        let pose = poses[i].pose;
+        for (let j = 0; j < pose.keypoints.length; j++) {
+            // A keypoint is an object describing a body part (like rightArm or leftShoulder)
+            let keypoint = pose.keypoints[j];
+            // Only draw an ellipse is the pose probability is bigger than 0.2
+            if (keypoint.score > 0.2) {
+                fill(255);
+                stroke(20);
+                strokeWeight(4);
+                ellipse(round(keypoint.position.x), round(keypoint.position.y), 8, 8);
+            }
+        }
+    }
+}
+
+// A function to draw the skeletons
+
+function drawSkeleton() {
+    // Loop through all the skeletons detected
+    for (let i = 0; i < poses.length; i++) {
+        let skeleton = poses[i].skeleton;
+        // For every skeleton, loop through all body connections
+        for (let j = 0; j < skeleton.length; j++) {
+            let partA = skeleton[j][0];
+            let partB = skeleton[j][1];
+            stroke(255);
+            strokeWeight(1);
+            line(partA.position.x, partA.position.y, partB.position.x, partB.position.y);
+        }
+    }
+}
